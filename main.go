5a31c05e18cbb65f37192d82e283f21a938d65fc
DavidPiedra1 <InsanityMatrix@gmail.com>
2020-02-06 23:27:31 -0500

    Bug fixing?

--- main.go
+++ main.go
@@ -100,8 +100,8 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 
     //Get the information about the user from user info
     user.username = r.Form.Get("username")
-    user.gender, _ = strconv.ParseBool(r.Form.Get("gender"))
-    user.age, _ = strconv.Atoi(r.Form.Get("age"))
+  //  user.gender, _ = strconv.ParseBool(r.Form.Get("gender"))
+    user.age = 16
     user.password = r.Form.Get("password")
 		cpassword := r.Form.Get("cpassword")
     user.email = r.Form.Get("email")
--- store.go
+++ store.go
@@ -16,30 +16,30 @@ type dbStore struct {
 }
 
 func (store *dbStore) CreateUser(user *User) error {
-	_, err := store.db.Query("INSERT INTO users(username,gender,age,password,email) VALUES ($1,$2,$3,$4,$5)",user.username,user.gender,user.age,user.password,user.email)
+	_, err := store.db.Query("INSERT INTO users(username,age,password,email) VALUES ($1,$2,$3,$4);",user.username,user.age,user.password,user.email)
 	return err
 }
 
 func (store *dbStore) GetUsers() ([]*User, error) {
 	rows, err := store.db.Query("SELECT username,gender,age,password,email from users")
-	
+
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	
+
 	users := []*User{}
-	
+
 	for rows.Next() {
 		user := &User{}
-		
+
 		if err := rows.Scan(&user.username, &user.gender, &user.age, &user.password, &user.email); err != nil {
 			return nil, err
 		}
-		
+
 		users = append(users, user)
 	}
-	
+
 	return users, nil
 }
 
