9337ab16a3bcb7e688f7626004628e406347adda
DavidPiedra1 <InsanityMatrix@gmail.com>
2020-02-11 21:43:22 -0500

    Fixed a column name

--- store.go
+++ store.go
@@ -96,7 +96,7 @@ func (store *dbStore) SetUserPublicity(userID int, mode bool) bool {
   return true
 }
 func (store *dbStore) ChangeUserEmail(userID int, email string) bool {
-  _, err := store.db.Query("UPDATE users SET email=$1 WHERE userid=$2",email, userID)
+  _, err := store.db.Query("UPDATE users SET email=$1 WHERE id=$2",email, userID)
   if err != nil {
     return false
   }
