7ddd3c0bbb8d79a46cb2d96fdc6ea17276087434
InsanityMatrix <39423022+InsanityMatrix@users.noreply.github.com>
2020-02-18 14:46:38 -0500

    started work on search page

---
+++ assets/scripts/search.js
@@ -0,0 +1,20 @@
+$("input[name='search']").keypress(function(e){
+    var searchValue= $(this).val();
+    searchFor(searchValue);
+});
+
+function searchFor(searchValue) {
+    //DO AJAX HERE
+    $.ajax({
+		url: "/search",
+		type: 'POST',
+		data: {
+			"term": searchValue
+		},
+		success: resultsSuccess
+	});
+}
+
+function resultsSuccess(data) {
+    //Parse JSON and display results
+}
