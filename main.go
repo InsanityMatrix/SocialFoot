5f1cc600d632935ffefbf26c8a9f381e5b6eea8a
DavidPiedra1 <InsanityMatrix@gmail.com>
2020-02-18 19:55:50 -0500

    better error handling

--- main.go
+++ main.go
@@ -106,7 +106,7 @@ func main() {
     router := newRouter()
     portEnv := os.Getenv("PORT")
     port := ":" + portEnv
-		HOME = os.Getenv("HOME") + "/go/src/github.com/InsanityMatrix/SocialFoot/"
+		HOME = os.Getenv("HOME") + "/go/src/github.com/InsanityMatrix/SocialFoot"
 
 		url := os.Getenv("DATABASE_URL")
 		db, err := sql.Open("postgres", url)
@@ -218,6 +218,7 @@ func liveIndexHandler(w http.ResponseWriter, r *http.Request) {
 
 	pubposts, err := store.GetPublicPosts()
 	if err != nil {
+		tmpl.Execute(w, FeedData{username:msg.Value})
 		return
 	}
 	feed := []LiveImagePost{}
