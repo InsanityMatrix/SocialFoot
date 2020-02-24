ed75c11f19452e7aae88a2f47d966e9fe4fc43a8
InsanityMatrix <39423022+InsanityMatrix@users.noreply.github.com>
2020-02-24 14:00:26 -0500

    fix only replacing one occurrence in template with javascript

--- assets/scripts/SocialFoot.js
+++ assets/scripts/SocialFoot.js
@@ -2,7 +2,7 @@ function executeHTMLTemplate(template, data) {
   var dataLength = Object.keys(data).length;
   for (var i = 0; i < dataLength; i++) {
     var key = Object.keys(data)[i];
-    if (template.includes("{{." + key +"}}")) {
+    while (template.includes("{{." + key +"}}")) {
       template = template.replace("{{." + key + "}}", data[key]);
     }
   }
