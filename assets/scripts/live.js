974d2997c6b810bab502d5649aabbb65a796842f
DavidPiedra1 <InsanityMatrix@gmail.com>
2020-02-27 10:02:24 -0500

    oops

--- assets/scripts/live.js
+++ assets/scripts/live.js
@@ -105,3 +105,70 @@ $(document).ready(function() {
     }
   });
 });
+function getUserPosts() {
+  if(postsRec) {
+    return;
+  }
+  var uid = parseInt(document.getElementById("profileUserId").innerHTML);
+  $.ajax({
+    url: '/live/user/posts',
+    method: 'POST',
+    data: {
+      "uid": uid
+    },
+    success: function(data) {
+      postsRec = true;
+      var length = data.length;
+      $("#posts").html("");
+      for(var i = 0; i < length; i++) {
+        var imageLink = "/assets/uploads/imageposts/post" + data[i].postid + data[i].extension;
+        var stuff = $("#posts").html();
+        //Parse Date
+        var d = new Date(data[i].posted);
+        var dd = d.getDate();
+        var mm = d.getMonth() + 1;
+        var yyyy = d.getFullYear();
+        if (dd < 10) {
+          dd = '0' + dd;
+        }
+        if (mm < 10) {
+          mm = '0' + mm;
+        }
+        var thisdate = mm + '/' + dd + '/' + yyyy;
+        //get tags with regexp
+        var tagpattern = /(#\w+)/g;
+        var m;
+        tagstext = ""
+        do {
+          m = tagpattern.exec(data[i].tags);
+          if (m) {
+            tagstext += "<p class='postTag'>" + m[1] + "</p>";
+          }
+        } while (m);
+
+        var postData = {
+          "userid":data[i].userid,
+          "thisdate":thisdate,
+          "imageLink":imageLink,
+          "tags": tagstext,
+          "caption":data[i].caption,
+          "postid":data[i].postid
+        };
+        var parsedTemplate = executeHTMLTemplate(template, postData);
+        $("#posts").html(stuff + parsedTemplate);
+        $.ajax({
+          url: '/json/user/id',
+          method: 'POST',
+          data : {
+            "userid": data[i].userid
+          },
+          success: putPostUsernames
+        });
+    }
+  }
+});
+  function putPostUsernames(data) {
+    var id = data[0].id;
+    $('p[id="' + id + '"]').html(data[0].username);
+  }
+}
