65da866b70430d3139193136d6a256b4e34098cd
DavidPiedra1 <InsanityMatrix@gmail.com>
2020-03-21 18:47:19 -0400

    Make it a bit better

--- assets/scripts/games/snake.js
+++ assets/scripts/games/snake.js
@@ -4,7 +4,8 @@ var paused = false;
 var grid = 16;
 var count = 0;
 var userid = 0;
-var moves = 0;
+var movesV = 0;
+var movesH = 0;
 
 var snake = {
   x: 160, y: 160, dx: grid, dy: 0,cells: [], maxCells: 4
@@ -65,7 +66,8 @@ function loop() {
   if (++count < 6) {
     return;
   }
-  moves = 0;
+  movesH = 0;
+  movesV = 0;
   count = 0;
   context.clearRect(0,0,canvas.width,canvas.height);
   snake.x += snake.dx;
@@ -116,39 +118,39 @@ function loop() {
 document.addEventListener('keydown', function(e) {
   // left arrow key
   if (e.which === 37 && snake.dx === 0) {
-    if(moves > 0) {
+    if(movesH > 0) {
       return;
     }
     snake.dx = -grid;
     snake.dy = 0;
-    moves++;
+    movesH++;
   }
   // up arrow key
   else if (e.which === 38 && snake.dy === 0) {
-    if(moves > 0) {
+    if(movesV > 0) {
       return;
     }
     snake.dy = -grid;
     snake.dx = 0;
-    moves++;
+    movesV++;
   }
   // right arrow key
   else if (e.which === 39 && snake.dx === 0) {
-    if(moves > 0) {
+    if(movesH > 0) {
       return;
     }
     snake.dx = grid;
     snake.dy = 0;
-    moves++;
+    movesH++;
   }
   // down arrow key
   else if (e.which === 40 && snake.dy === 0) {
-    if(moves > 0) {
+    if(movesV > 0) {
       return;
     }
     snake.dy = grid;
     snake.dx = 0;
-    moves++;
+    movesV++;
   }
   else if (e.which === 32) {
     if(paused) {
